5fa9b6a31f3cde1579a38be7ff2b49c931752648
Fall <81432164+Bladeswillfall@users.noreply.github.com>
2026-09-07 13:58:05 +0100

    Register pulley on Fabric

--- fabric/src/main/java/io/github/bladeswillfall/spelunkingrope/fabric/SpelunkingRopeFabric.java
+++ fabric/src/main/java/io/github/bladeswillfall/spelunkingrope/fabric/SpelunkingRopeFabric.java
@@ -4,6 +4,7 @@ import io.github.bladeswillfall.spelunkingrope.SpelunkingRope;
 import io.github.bladeswillfall.spelunkingrope.rope.GuideClipBlock;
 import io.github.bladeswillfall.spelunkingrope.rope.GuideCordItem;
 import io.github.bladeswillfall.spelunkingrope.rope.PitonBlock;
+import io.github.bladeswillfall.spelunkingrope.rope.PulleyBlock;
 import io.github.bladeswillfall.spelunkingrope.rope.RopeCoilItem;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.core.Registry;
@@ -24,6 +25,10 @@ public final class SpelunkingRopeFabric implements ModInitializer {
         ResourceLocation guideClipId = new ResourceLocation(SpelunkingRope.MOD_ID, "guide_clip");
         GuideClipBlock guideClip = Registry.register(BuiltInRegistries.BLOCK, guideClipId, new GuideClipBlock());
         Registry.register(BuiltInRegistries.ITEM, guideClipId, new BlockItem(guideClip, new Item.Properties()));
+
+        ResourceLocation pulleyId = new ResourceLocation(SpelunkingRope.MOD_ID, "pulley");
+        PulleyBlock pulley = Registry.register(BuiltInRegistries.BLOCK, pulleyId, new PulleyBlock());
+        Registry.register(BuiltInRegistries.ITEM, pulleyId, new BlockItem(pulley, new Item.Properties()));
 
         Registry.register(
                 BuiltInRegistries.ITEM,
