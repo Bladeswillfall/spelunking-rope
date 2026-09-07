3b0afda151f8763a445456dbaf01f33007984a6c
Fall <81432164+Bladeswillfall@users.noreply.github.com>
2026-09-07 13:33:31 +0100

    Test guide dye math without registry bootstrap

--- common/src/test/java/io/github/bladeswillfall/spelunkingrope/rope/GuideCordItemTest.java
+++ common/src/test/java/io/github/bladeswillfall/spelunkingrope/rope/GuideCordItemTest.java
@@ -1,31 +1,23 @@
 package io.github.bladeswillfall.spelunkingrope.rope;
 
 import net.minecraft.world.item.DyeColor;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GuideCordItemTest {
     @Test
-    void resolvesUndyedAndAllVanillaDyeColours() {
-        GuideCordItem item = new GuideCordItem(new Item.Properties(), ignored -> { });
-        ItemStack stack = new ItemStack(item);
-
-        assertEquals(FixedRopeSnapshot.NO_DYE, GuideCordItem.dyeId(stack));
+    void resolvesAllVanillaDyeColours() {
         for (DyeColor color : DyeColor.values()) {
-            item.setColor(stack, GuideCordItem.dyeRgb((byte) color.getId()));
-            assertEquals((byte) color.getId(), GuideCordItem.dyeId(stack));
+            assertEquals(
+                    (byte) color.getId(),
+                    GuideCordItem.closestDyeId(GuideCordItem.dyeRgb((byte) color.getId()))
+            );
         }
     }
 
     @Test
     void mixedRgbChoosesNearestMinecraftDye() {
-        GuideCordItem item = new GuideCordItem(new Item.Properties(), ignored -> { });
-        ItemStack stack = new ItemStack(item);
-        item.setColor(stack, 0xFF1010);
-
-        assertEquals((byte) DyeColor.RED.getId(), GuideCordItem.dyeId(stack));
+        assertEquals((byte) DyeColor.RED.getId(), GuideCordItem.closestDyeId(0xFF1010));
     }
 }
